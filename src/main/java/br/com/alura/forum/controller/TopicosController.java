d97e3577297b745e95b57d706c5e342d11b31c21
Lucas Funchal da Silva <lf10funchal@gmail.com>
2020-08-27 15:01:02 -0300

    Publicando endpoints

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+@RestController
+public class TopicosController {
+	
+	@RequestMapping("/topicos")
+	public List<TopicoDto> lista(){
+		Topico topico = new Topico("Dúvida", "Dúvida com spring", new Curso("Spring", "Programação"));
+		return TopicoDto.convert(Arrays.asList(topico, topico, topico));
+	}
+}
